03019fdbfa5b3f62ee9512605d01d7595d9f4553
paubel <paul.belfrage@gmail.com>
2022-10-01 18:54:24 +0200

    first commit

---
+++ astroData.js
@@ -0,0 +1,47 @@
+export default {
+  success: true,
+  data: {
+    constellations: [
+      {
+        id: "ESO 376-16",
+        constellations: "Antilia",
+        name: "ESO 376-16 - late-type spiral or a dwarf irregular galaxy",
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/ESO_376-16.jpg/512px-ESO_376-16.jpg",
+        width: 300,
+        height: 300,
+      },
+      {
+        id: "NGC 2997",
+        constellations: "Antilia",
+        name: "NGC 2997 - a face-on unbarred spiral galaxy",
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/N2997s.jpg/512px-N2997s.jpg",
+        width: 300,
+        height: 300,
+      },
+      {
+        id: "M31",
+        constellations: "Andromeda",
+        name: "M31 - The Andromeda Galaxy",
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Andromeda_Galaxy_560mm_FL.jpg/512px-Andromeda_Galaxy_560mm_FL.jpg",
+        width: 300,
+        height: 300,
+      },
+      {
+        id: "NGC 7662",
+        constellations: "Andromeda",
+        name: "NGC 7662 - Snowball Nebula",
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/NGC_7662_%22Blue_Snowball%22.jpg/512px-NGC_7662_%22Blue_Snowball%22.jpg",
+        width: 300,
+        height: 300,
+      },
+      {
+        id: "NGC 891",
+        constellations: "Andromeda",
+        name: "NGC 891 - Silver Sliver Galaxy",
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/NGC891HunterWilson.jpg/512px-NGC891HunterWilson.jpg",
+        width: 300,
+        height: 300,
+      },
+    ],
+  },
+};
