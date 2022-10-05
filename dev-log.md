504c7377123e4fdbc31255c36c4bdb09692873fd
paubel <paul.belfrage@gmail.com>
2022-10-05 12:15:01 +0200

    three diff cat

---
+++ dev-log.md
@@ -0,0 +1,9 @@
+Emission Nebulae
+Reflection Nebulae
+Dark nebulae / (Absorbtion nebulae)
+Planetary nebulae
+Supernova remnant
+
+Diffrent category: H II regions, large diffuse nebulae containing ionized hydrogen
+
+http://astroa.physics.metu.edu.tr/twn/types.html
--- scriptAstro.js
+++ scriptAstro.js
@@ -3,115 +3,85 @@
 import data from "./astroData.json" assert { type: "json" };
 /* console.log(data.astroObject); */
 
-const selectElement = document.querySelector("#astro-type");
+const selectElementType = document.querySelector("#astro-type");
 let astroType = "";
 
-selectElement.addEventListener("change", (event) => {
+selectElementType.addEventListener("change", (event) => {
   let node = document.querySelector("main");
   node.querySelectorAll("*").forEach((n) => n.remove());
   astroType = event.target.value;
-  populateObjects(astroType);
+  populateType(astroType);
 });
 
-/* async function populate() {
-  const requestURL = "./astroData.json";
-  const request = new Request(requestURL);
-  const response = await fetch(request);
-  const astroDataText = await response.text();
-  const astroData = JSON.parse(astroDataText);
+const selectElementConst = document.querySelector("#astro-const");
+let astroConst = "";
 
-  console.log(astroData);
-  return astroData;
-  //populateConstellations(astroData);
-  //populateObjects(astroData);
-  //   populateHeroes(superHeroes);
-} */
+selectElementConst.addEventListener("change", (event) => {
+  let node = document.querySelector("main");
+  node.querySelectorAll("*").forEach((n) => n.remove());
+  astroConst = event.target.value;
+  populateConst(astroConst);
+});
 
-function populateObjects(astroType) {
+const selectElementNebula = document.querySelector("#astro-nebula");
+let astroNebula = "";
+
+selectElementNebula.addEventListener("change", (event) => {
+  let node = document.querySelector("main");
+  node.querySelectorAll("*").forEach((n) => n.remove());
+  astroNebula = event.target.value;
+  populateNebula(astroNebula);
+});
+
+function populateType(astroType) {
   const main = document.querySelector("main");
   const myH2 = document.createElement("h2");
 
   myH2.textContent = astroType;
   main.appendChild(myH2);
 
-  /*   const astroData = populate(); */
-
-  /*   const printAstroData = async () => {
-    const a = await astroData;
-    console.log(a);
-  }; */
-
   Object.entries(data.astroObject).forEach(([key, value]) => {
-    /*   console.log(key, value);
-    console.log(value.type); */
-    console.log(value.type, astroType);
-    if (value.type === astroType) {
+    const words = value.type.split(" ");
+    //console.log(words[1], astroType.toLowerCase());
+
+    //console.log("Before: " + value.type, astroType, words[1]);
+    if (words[1].toLowerCase() === astroType.toLowerCase()) {
+      //console.log("After: " + value.type, astroType);
       const myImg = document.createElement("img");
       myImg.src = `${value.url}`;
       main.appendChild(myImg);
     }
   });
-  /*   for (let key in data.astroObject) {
-    console.log();
-     if (data.astroObject[i]) {
-      const myImg = document.createElement("img");
-      myImg.src = `${obj.astroObject[0].url}`;
-    } 
-  } */
 }
 
-function populateConstellations(obj) {
+function populateConst(astroConst) {
   const main = document.querySelector("main");
-  const myH1 = document.createElement("h1");
-  //console.log(obj.astroObject[0].constellations);
+  const myH2 = document.createElement("h2");
 
-  myH1.textContent = obj.astroObject[0].constellations;
-  main.appendChild(myH1);
+  myH2.textContent = astroConst;
+  main.appendChild(myH2);
 
-  const myPara = document.createElement("p");
-  myPara.textContent = `Name: ${obj.astroObject[0].name} `;
-  main.appendChild(myPara);
-
-  const myImg = document.createElement("img");
-  myImg.src = `${obj.astroObject[0].url}`;
-
-  main.appendChild(myImg);
-}
-
-function populateType(type) {}
-
-function populateHeroes(obj) {
-  const section = document.querySelector("section");
-  const heroes = obj.members;
-
-  for (const hero of heroes) {
-    const myArticle = document.createElement("article");
-    const myH2 = document.createElement("h2");
-    const myPara1 = document.createElement("p");
-    const myPara2 = document.createElement("p");
-    const myPara3 = document.createElement("p");
-    const myList = document.createElement("ul");
-
-    myH2.textContent = hero.name;
-    myPara1.textContent = `Secret identity: ${hero.secretIdentity}`;
-    myPara2.textContent = `Age: ${hero.age}`;
-    myPara3.textContent = "Superpowers:";
-
-    const superPowers = hero.powers;
-    for (const power of superPowers) {
-      const listItem = document.createElement("li");
-      listItem.textContent = power;
-      myList.appendChild(listItem);
+  Object.entries(data.astroObject).forEach(([key, value]) => {
+    if (value.constellations === astroConst) {
+      const myImg = document.createElement("img");
+      myImg.src = `${value.url}`;
+      main.appendChild(myImg);
     }
-
-    myArticle.appendChild(myH2);
-    myArticle.appendChild(myPara1);
-    myArticle.appendChild(myPara2);
-    myArticle.appendChild(myPara3);
-    myArticle.appendChild(myList);
-
-    section.appendChild(myArticle);
-  }
+  });
 }
 
-/* populate(); */
+function populateNebula(astroNebula) {
+  const main = document.querySelector("main");
+  const myH2 = document.createElement("h2");
+
+  myH2.textContent = astroNebula;
+  main.appendChild(myH2);
+
+  Object.entries(data.astroObject).forEach(([key, value]) => {
+    if (value.type === astroNebula) {
+      const myImg = document.createElement("img");
+      myImg.src = `${value.url}`;
+      main.appendChild(myImg);
+    }
+  });
+}
