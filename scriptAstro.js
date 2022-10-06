e80407755f58325ec33173dcd8bec933e1872be2
paubel <paul.belfrage@gmail.com>
2022-10-06 10:37:33 +0200

    change to fetch from import

--- scriptAstro.js
+++ scriptAstro.js
@@ -1,74 +1,80 @@
 "use strict";
-import data from "./astroData.json" assert { type: "json" };
+//import data from "./astroData.json" assert { type: "json" }; Do not work in FF
 
-const selectElementType = document.querySelector("#astro-type");
-const selectElementConst = document.querySelector("#astro-const");
-let main = null;
-let myH2 = null;
-let selectedOption = "";
+fetch("./astroData.json")
+  .then((res) => res.json())
+  .then((data) => {
+    // do stuff with the data
 
-selectElementType.addEventListener("change", (event) => {
-  resetSelectElement(selectElementConst);
-  clearMainAndSelectOption();
-  populateType(selectedOption);
-});
+    const selectElementType = document.querySelector("#astro-type");
+    const selectElementConst = document.querySelector("#astro-const");
+    let main = null;
+    let myH2 = null;
+    let selectedOption = "";
 
-selectElementConst.addEventListener("change", (event) => {
-  resetSelectElement(selectElementType);
-  clearMainAndSelectOption();
-  populateConst(selectedOption);
-});
+    selectElementType.addEventListener("change", (event) => {
+      resetSelectElement(selectElementConst);
+      clearMainAndSelectOption();
+      populateType(selectedOption);
+    });
 
-function populateType(astroData) {
-  createH2inMain(astroData);
+    selectElementConst.addEventListener("change", (event) => {
+      resetSelectElement(selectElementType);
+      clearMainAndSelectOption();
+      populateConst(selectedOption);
+    });
 
-  Object.entries(data.astroObject).forEach(([key, value]) => {
-    const words = value.type.split(" ");
-    /*     console.log(value.type, astroData);
+    function populateType(astroData) {
+      createH2inMain(astroData);
+
+      Object.entries(data.astroObject).forEach(([key, value]) => {
+        const words = value.type.split(" ");
+        /*     console.log(value.type, astroData);
     console.log(words[1].toLowerCase(), astroData.toLowerCase());
     console.log("-------------"); */
-    if (words[1].toLowerCase() === astroData.toLowerCase()) {
-      createAstroElement(value);
-    } else if (value.type === astroData) {
-      createAstroElement(value);
+        if (words[1].toLowerCase() === astroData.toLowerCase()) {
+          createAstroElement(value);
+        } else if (value.type === astroData) {
+          createAstroElement(value);
+        }
+      });
+    }
+
+    function populateConst(astroData) {
+      createH2inMain(astroData);
+
+      Object.entries(data.astroObject).forEach(([key, value]) => {
+        if (value.constellations === astroData) {
+          createAstroElement(value);
+        }
+      });
+    }
+
+    function clearMainAndSelectOption() {
+      let node = document.querySelector("main");
+      node.querySelectorAll("*").forEach((n) => n.remove());
+      selectedOption = event.target.value;
+    }
+
+    function createH2inMain(astroData) {
+      main = document.querySelector("main");
+      myH2 = document.createElement("h2");
+      myH2.textContent = astroData;
+      main.appendChild(myH2);
+    }
+
+    function createAstroElement(value) {
+      const myFigure = document.createElement("figure");
+      const myImg = document.createElement("img");
+      const myFigcaption = document.createElement("figcaption");
+      myImg.src = `${value.url}`;
+      myFigcaption.textContent = `${value.id} ${value.name} in ${value.constellations}`;
+      main.appendChild(myFigure);
+      myFigure.appendChild(myImg);
+      myFigure.appendChild(myFigcaption);
+    }
+
+    function resetSelectElement(selectElement) {
+      selectElement.selectedIndex = 0; //
     }
   });
-}
-
-function populateConst(astroData) {
-  createH2inMain(astroData);
-
-  Object.entries(data.astroObject).forEach(([key, value]) => {
-    if (value.constellations === astroData) {
-      createAstroElement(value);
-    }
-  });
-}
-
-function clearMainAndSelectOption() {
-  let node = document.querySelector("main");
-  node.querySelectorAll("*").forEach((n) => n.remove());
-  selectedOption = event.target.value;
-}
-
-function createH2inMain(astroData) {
-  main = document.querySelector("main");
-  myH2 = document.createElement("h2");
-  myH2.textContent = astroData;
-  main.appendChild(myH2);
-}
-
-function createAstroElement(value) {
-  const myFigure = document.createElement("figure");
-  const myImg = document.createElement("img");
-  const myFigcaption = document.createElement("figcaption");
-  myImg.src = `${value.url}`;
-  myFigcaption.textContent = `${value.id} ${value.name} in ${value.constellations}`;
-  main.appendChild(myFigure);
-  myFigure.appendChild(myImg);
-  myFigure.appendChild(myFigcaption);
-}
-
-function resetSelectElement(selectElement) {
-  selectElement.selectedIndex = 0; //
-}
